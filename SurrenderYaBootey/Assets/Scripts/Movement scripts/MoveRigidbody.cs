638df65651cf5daab99a22013306e723144414c2
kallesongotto <47325654+kallesongotto@users.noreply.github.com>
2020-09-10 13:28:06 +0200

    Work with movement in Otto Scene
    By Otto and Kevin

---
+++ SurrenderYaBootey/Assets/Scripts/Movement scripts/MoveRigidbody.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRigidbody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
+
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 tempVect = new Vector3(h, 0, v);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + tempVect);
+    }
+}
